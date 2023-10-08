dba0ba8d16ba71f0aefd9f30e2af468ab9a38b1c
Aaron Imbrock <aimbrock@gmail.com>
2023-10-08 12:33:15 -0700

    static, uploads working

--- app/django_project/urls.py
+++ app/django_project/urls.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from upload.views import image_upload
 
 urlpatterns = [
+    path("", include("pages.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include("pages.urls")),
+    path("upload/", image_upload, name="upload"),
 ]
 
 if settings.DEBUG:
@@ -14,3 +18,4 @@ if settings.DEBUG:
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
